11d9817d4b0285f3e689dba62d93d7adb9c50cff
agnesjuliana <agafordev@gmail.com>
2025-12-30 05:59:42 +0700

    fix: soa validator

--- src/validators/soa/soa.validator.ts
+++ src/validators/soa/soa.validator.ts
@@ -28,6 +28,12 @@ export const updateSOASchema = Joi.object({
     .messages({
       'any.only': 'Status hanya bisa BELUM_DITENTUKAN, RELEVAN, TIDAK_RELEVAN, atau DIHENTIKAN',
     }),
+  implementationStatus: Joi.string()
+    .valid('DIRENCANAKAN', 'DALAM_IMPLEMENTASI', 'DIIMPLEMENTASIKAN', 'DIHENTIKAN')
+    .optional()
+    .messages({
+      'any.only': 'Implementation Status hanya bisa DIRENCANAKAN, DALAM_IMPLEMENTASI, DIIMPLEMENTASIKAN, atau DIHENTIKAN',
+    }),
   notes: Joi.string().optional().allow(null),
   targetDate: Joi.date().optional().allow(null),
 });
